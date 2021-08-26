3f7266071dba8c4dd35bbca409178c7eb3c401bc
bwesterg <benton.westergaard@gmail.com>
2021-08-26 13:30:34 -0600

    testing lodash/fp

--- src/01-simple-debug.ts
+++ src/01-simple-debug.ts
@@ -1,4 +1,12 @@
+import { map } from "lodash/fp"
+
 console.log('Simple debugging example running.')
+
+type THIS_IS_DUMB_REFACTOR_ME = any
+
+const doubledStuff = map((x: number) => {
+    return x * 2
+})([2])
 
 function double(someNumbers: number[]): number[] {
     return someNumbers.map(someNumber => {
